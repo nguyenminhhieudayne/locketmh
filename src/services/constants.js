7dabd998f49daded8ca93f725776270b6ae431a5
nguyenminhhieudayne <108606614+nguyenminhhieudayne@users.noreply.github.com>
2024-11-06 03:27:21 +0700

    Update constants.js

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,8 +1,8 @@
 const constants = {
     apiRoutes: {
-        LOGIN_URL: "https://locketuploader-be-render.onrender.com/locket/login",
+        LOGIN_URL: "https://phunguyn-locket-zmmw.onrender.com/locket/login",
         UPLOAD_MEDIA_URL:
-            "https://locketuploader-be-render.onrender.com/locket/upload-media",
+            "https://phunguyn-locket-zmmw.onrender.com/locket/upload-media",
     },
     toastSettings: {
         position: "bottom-right",
